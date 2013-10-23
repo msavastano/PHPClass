44f3efcec9a4c231da9aabcb98ae818b79bd9b04
millhouse5000 <millhouse5000@gmail.com>
2013-10-23 19:39:34 -0400

    week 3 commit

---
+++ week3/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        //$_POST
+        ?>
+        
+        <form name="mainform" action="index.php" method="post">
+            <input type="text" name="fullname" value="" /> <br />
+            <input type="submit" value="submit" />            
+        </form>
+    </body>
+</html>
