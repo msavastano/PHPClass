dbc14d2daa4f5131c702f046078fcc9b3433af6c
msavastano <millhouse5000@gmail.com>
2013-11-17 12:37:16 -0500

    Final - Saas Project

--- Final/class/Validator.php
+++ Final/class/Validator.php
@@ -25,6 +25,38 @@ class Validator {
            return true;
        }
        return false; 
-}
+    }
+    
+     //validates whether POST data exists
+     public static function loginIsValidPost() {
+          if ( !array_key_exists("username", $_POST) 
+                || !array_key_exists("password", $_POST) ) {
+               return false;
+          }          
+          return Validator::loginIsValid($_POST["username"],$_POST["password"] ); //sends into reusable validator
+     }
+     //validates against strings
+      public static function loginIsValid( $email, $password ) {
+        //static class cannot call its own class with '$this->'
+           if( !Validator::emailIsValid($email) 
+                    || !Validator::passwordIsValid($password) ) {
+             return false;             
+           }  //checks agaist data in database
+        $password = sha1($password);
+        $dbCls = new DB();
+        $db = $dbCls->getDB();
+        if ( NULL != $db ) {
+            $stmt = $db->prepare('select * from users where email = :emailValue and password = :passwordValue limit 1');
+            $stmt->bindParam(':emailValue', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':passwordValue', $password, PDO::PARAM_STR);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+           
+            if (is_array($result) && count($result) ){                
+                return true;
+            }            
+        }        
+        return false;      
+    }
 }
 
---
+++ Final/login.php
@@ -0,0 +1,66 @@
+<?php include 'dependency.php'; //includes all classes and sets up session ?> 
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login - Final</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    <body>
+        <?php
+        
+        //TEST CODE
+        //print_r($_SESSION);
+        //echo "</br />";
+        //echo "POST ";
+        //print_r($_POST);
+        
+        //set bad login message var
+        $err = "";
+        print_r($_SESSION);
+        print_r($_POST);
+        //check for username and password validation with class.  Set session var and err mess
+        if (isset($_POST['username']) ){
+            if ( Validator::loginIsValidPost() ) { 
+                $_SESSION['username'] = $_POST['username'];
+                $_SESSION["isLoggedIn"] = true; 
+            }else {
+                $_SESSION["isLoggedIn"] = false; 
+                $err = "Username or Password incorrect";                
+            }
+        }  
+        
+      //if session var is 1, redirect to admin page  
+      if ( isset($_SESSION["isLoggedIn"]) && $_SESSION["isLoggedIn"] == true ) {
+        //header("Location: admin.php");
+      }   
+      //avoid session hijack using tokens - token set on dependency page
+        if( !isset($_SESSION['token']) ){
+            $_SESSION['token'] = $token;
+        }else{
+            if ( isset($_POST['token']) && $_SESSION['token'] != $_POST['token'] ){
+                session_destroy();
+                header("Location:login.php");
+                exit();
+            }
+        }
+        //end php
+        ?> 
+        
+        
+        <h1 id="h">Login</h1>
+        <div id="divOne">
+        <form name="mainform" action="login.php" method="post">            
+            Username/Email: <input type="text" name="email" /> <br /><br />
+            Password: <input type="password" name="password" /> <br />
+            <?php echo '<p id="err">', $err, '</p>'; ?> <!---error message, if set -->                  
+            <input type="submit" value="Submit" />            
+        </form>
+            
+        </div>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+        <script src="js/jscr.js" type="text/javascript"></script>
+    </body>
+</html>
